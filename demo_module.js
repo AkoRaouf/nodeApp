a9721dcb48880b8643591aa8188d118739a14dfd
acer <acer@DESKTOP-HMQQE1N>
2020-11-26 21:11:13 +0330

    first app in node js

--- demo_module.js
+++ demo_module.js
@@ -1,11 +1,17 @@
 var http = require('http');
 var dt = require('./myfirstModule');
 var fs = require('fs');
+var uc = require('upper-case');
 
 http.createServer(function (req, res) {
   fs.readFile('demofile1.html', function (err, data) {
+    if(err){
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        return res.end();
+    }
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
+    res.write(uc.upperCase('hello wrold'));
     return res.end();
   });
 
@@ -14,8 +20,8 @@ http.createServer(function (req, res) {
     console.log('Saved!');
   });
 
-  fs.unlink('newfile1.txt', function(err){
-    if(err) throw err;
-    console.log('File Deleted!');
-  });
+  // fs.unlink('newfile1.txt', function(err){
+  //   if(err) throw err;
+  //   console.log('File Deleted!');
+  // });
 }).listen(8080);
