69b16fe8af6fa128cf7d478546c0e2500bd88fab
acer <acer@DESKTOP-HMQQE1N>
2020-11-26 19:11:33 +0330

    add files

--- demo_module.js
+++ demo_module.js
@@ -1,9 +1,21 @@
 var http = require('http');
 var dt = require('./myfirstModule');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('The date and time are currently: '+ dt.myDateTime());
-  res.write(req.url);
-  res.end();
+  fs.readFile('demofile1.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  });
+
+  fs.appendFile('newfile1.txt', 'Hello Content', function(err){
+    if(err) throw err;
+    console.log('Saved!');
+  });
+
+  fs.unlink('newfile1.txt', function(err){
+    if(err) throw err;
+    console.log('File Deleted!');
+  });
 }).listen(8080);
